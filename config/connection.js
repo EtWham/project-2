694cdc8c0d48593bc4c76866137fa2e9fe6cd8ac
Ethan Wham <Ethan Wham>
2017-05-20 13:02:13 -0400

    updates

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+//requiring mysql
+var mysql = require("mysql");
+var connection;
+//still don't quite understand jawsDB just following the pdf
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		database: 'Users'
+	});
+
+}
+//checking the connection
+connection.connect(function(err){
+	if(err){
+		console.log(err);
+	}
+	else{
+		console.log('connected as id: ' + connection.threadId);
+	}
+});
+//exporting the connection
+module.exports = connection;
