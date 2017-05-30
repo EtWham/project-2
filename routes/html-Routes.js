a453981e32ec75d431414f9f5d0241efc2e005b6
EtWham <etwham@gmail.com>
2017-05-30 16:12:30 -0400

    routing & chatrooms work

--- routes/html-Routes.js
+++ routes/html-Routes.js
@@ -22,8 +22,28 @@ module.exports = function(app) {
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/survey.html"));
   });
-  //chat room route
+  //general chat room route
   app.get("/chat", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/chatroom.html"));
+  });
+  //coding chat room route
+  app.get("/coding", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/chatroom.html"));
+  });
+  //algebra chat room route
+  app.get("/algebra", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/chatroom.html"));
+  });
+  //calculus chat room route
+  app.get("/calculus", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/chatroom.html"));
+  });
+  //physics chat room route
+  app.get("/physics", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/chatroom.html"));
+  });
+  //history chat room route
+  app.get("/history", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/chatroom.html"));
   });
 };
